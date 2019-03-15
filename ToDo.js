8d5a532def8dfcedb04dbf036de065a8a60ad2df
begaonnuri <begaonnuri@gmail.com>
2019-03-15 22:06:32 +0900

    delete

--- ToDo.js
+++ ToDo.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 import { View, Text, TouchableOpacity, StyleSheet, Dimensions, TextInput } from "react-native";
+import PropTypes from 'prop-types';
 
 const { width, height } = Dimensions.get("window");
 
 export default class ToDo extends React.Component{
-    state = {
-        isEditing: false,
-        isCompleted: false,
-        toDoValue: ""
+    constructor(props){
+        super(props);
+        this.state = {
+            isEditing: false,
+            toDoValue: props.text
+        }
     }
+    static propTypes = {
+        text: PropTypes.string.isRequired,
+        isCompleted: PropTypes.bool.isRequired,
+        deleteToDo: PropTypes.func.isRequired,
+        id: PropTypes.string.isRequired
+    }
+    
     render(){
             const { isCompleted, isEditing, toDoValue } = this.state;
-            const { text } = this.props;
+            const { text, id, deleteToDo } = this.props;
         return (
             <View style={styles.container}>
                 <View style={styles.column}>
@@ -37,7 +47,7 @@ export default class ToDo extends React.Component{
                                 <Text style={styles.actionText}>pencil</Text>
                             </View>
                         </TouchableOpacity>
-                        <TouchableOpacity onPressOut={this._finishEditing}>
+                        <TouchableOpacity onPressOut={() => deleteToDo(id)}>
                             <View style={styles.actionContatiner}>
                                 <Text style={styles.actionText}>cancel</Text>
                             </View>
@@ -55,10 +65,8 @@ export default class ToDo extends React.Component{
         })
     };
     _startEditing = () =>{
-        const { text } = this.props;
         this.state({
-            isEditing: true,
-            toDoValue: text
+            isEditing: true
         })
     };
     _finishEditing = () =>{
@@ -109,8 +117,7 @@ const styles = StyleSheet.create({
     column:{
         flexDirection: "row",
         alignItems: "center",
-        width: width / 2,
-        justifyContent: "space-between"
+        width: width / 2
     },
     actions:{
         flexDirection: "row"
